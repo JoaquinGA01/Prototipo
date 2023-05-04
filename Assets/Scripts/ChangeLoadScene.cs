6721483bad42314f8e627586ac405f5f0dbe0a4a
JoaquinGA01 <otrebla.2817@gmail.com>
2023-05-04 11:17:42 -0600

    Actualizacion

--- Assets/Scripts/ChangeLoadScene.cs
+++ Assets/Scripts/ChangeLoadScene.cs
@@ -6,10 +6,15 @@ using UnityEngine.SceneManagement;
 public class ChangeLoadScene : MonoBehaviour
 {   
     public string nombre_modelo;
+    public float vectorx;
+    public float vectory;
+    public float vectorz;
     public void LoadScene(string sceneName){
         if(nombre_modelo!=null){
             PlayerPrefs.SetString("Nombre_Modelo", nombre_modelo);
- 
+            PlayerPrefs.SetFloat("VectorX", vectorx);
+            PlayerPrefs.SetFloat("VectorY", vectory);
+            PlayerPrefs.SetFloat("VectorZ", vectorz);
         }        
         SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
--- Assets/Scripts/ObtenerData.cs
+++ Assets/Scripts/ObtenerData.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObtenerData : MonoBehaviour
-{
-    public Vector3 posicion; //Posición en la que se quiere instanciar el modelo
+{ //Posición en la que se quiere instanciar el modelo
 
 void Start()
 {
     string nombre = PlayerPrefs.GetString("Nombre_Modelo");
     Debug.Log(nombre);
     GameObject modeloPrefab = Resources.Load<GameObject>(nombre);
-    GameObject modeloInstanciado = Instantiate(modeloPrefab, posicion, Quaternion.identity); //Se instancia el prefab del modelo en la posición especificada
+    GameObject modeloInstanciado = Instantiate(modeloPrefab, new Vector3(PlayerPrefs.GetFloat("VectorX"),PlayerPrefs.GetFloat("VectorY"),PlayerPrefs.GetFloat("VectorZ")), Quaternion.identity); //Se instancia el prefab del modelo en la posición especificada
 }
 }
