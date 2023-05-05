e392a3e84fb72f658ca709a564adec8172ac8856
JoaquinGA01 <otrebla.2817@gmail.com>
2023-05-04 23:23:38 -0600

    Version Funcional 1.1.0

--- Assets/Scripts/ObtenerData.cs
+++ Assets/Scripts/ObtenerData.cs
@@ -10,6 +10,6 @@ void Start()
     string nombre = PlayerPrefs.GetString("Nombre_Modelo");
     Debug.Log(nombre);
     GameObject modeloPrefab = Resources.Load<GameObject>(nombre);
-    GameObject modeloInstanciado = Instantiate(modeloPrefab, new Vector3(PlayerPrefs.GetFloat("VectorX"),PlayerPrefs.GetFloat("VectorY"),PlayerPrefs.GetFloat("VectorZ")), Quaternion.identity); //Se instancia el prefab del modelo en la posición especificada
+    GameObject modeloInstanciado = Instantiate(modeloPrefab, new Vector3(0,2,5), Quaternion.identity); //Se instancia el prefab del modelo en la posición especificada
 }
 }
